78c778122d9afd146cf20b55c745ac761d6aa894
Ben Cohen <ben@bencohendev.com>
2020-05-12 14:24:37 -0400

    Basic Router Function Working

--- src/App.js
+++ src/App.js
@@ -2,18 +2,48 @@ import React from 'react';
 import './css/App.css';
 import StaticToneGenerator from './components/StaticToneGenerator'
 import DynamicToneGenerator from './components/DynamicToneGenerator'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header 
-        className="header"
-      >
-      Welcome to Ben's Pitch Thingy
-      </header>
-  {/*<StaticToneGenerator />*/}
-      <DynamicToneGenerator />
-    </div>
+    <Router>
+      <div className="App">
+        <header 
+          className="header"
+        >
+        Welcome to Ben's Pitch Thingy
+        </header>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Static Pitches</Link>
+              </li>
+              <li>
+                <Link to="/dynamic">Pitch Series</Link>
+              </li>
+            </ul>
+          </nav>
+
+        <Switch>
+          <Route path="/dynamic">
+            <DynamicToneGenerator />
+          </Route>
+          <Route path="/">
+            <StaticToneGenerator />
+          </Route>
+        </Switch>
+
+
+        </div>
+    </Router>
+    // <div className="App">
+
+    // </div>
   );
 }
 
