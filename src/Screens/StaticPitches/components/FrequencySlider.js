93492da35d1a784498af7555613e0394149fbd32
Ben Cohen <ben@bencohendev.com>
2020-06-05 13:54:27 -0400

    reducer added to static only. components separated to accommodate

---
+++ src/Screens/StaticPitches/components/FrequencySlider.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Slider } from '@material-ui/core';
+import '../../../App.css'
+
+
+export const FrequencySlider = (props) => {
+
+  const { nodes, i, dispatch } = props
+  const oscillator = i ? nodes[i] : nodes[0]
+  const frequency = oscillator.frequency
+  return (
+    <div>
+      <p className="field-label">Frequency Slider </p>
+      <Slider
+        aria-labelledby="continuous-slider"
+        min={3}
+        max={14.5}
+        step={.001}
+        value={Math.log2(frequency)}
+        onChange={(e, value) => dispatch(e, value, i)}
+        className='frequency'
+      />
+      <div>{Math.round((frequency + Number.EPSILON) * 1000) / 1000}</div>
+    </div>
+  )
+}
